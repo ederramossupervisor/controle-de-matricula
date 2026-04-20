cb9dd6200ec8ede5d01da09c4fafff9967e9e322
ederramossupervisor <eder.ramos@educador.edu.es.gov.br>
2026-04-19 21:31:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2189,6 +2189,9 @@ async function carregarAlunos(pagina = 1, filtros = {}) {
     }
 
     perfilUsuario = dados.perfil;
+    document.body.classList.remove('perfil-supervisor', 'perfil-secretaria');
+    document.body.classList.add(perfilUsuario === 'SUPERVISOR' ? 'perfil-supervisor' : 'perfil-secretaria');
+    
     escolaUsuario = dados.escola;
 
     document.getElementById("escolaUsuarioDisplay").textContent = 
