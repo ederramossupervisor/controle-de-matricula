739872ae0b280f7c8753ff0ceb8b9a2c42cea6e0
ederramossupervisor <eder.ramos@educador.edu.es.gov.br>
2026-04-21 19:10:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -210,6 +210,19 @@ let modoVisualizacao = 'cards'; // 'cards' ou 'lista'
 // =========================
 const CACHE_TTL = 30 * 60 * 1000; // 30 minutos
 
+function aplicarMascaraCPF(input) {
+  let valor = input.value.replace(/\D/g, '');
+  if (valor.length > 11) valor = valor.slice(0, 11);
+  if (valor.length > 9) {
+    valor = valor.replace(/^(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+  } else if (valor.length > 6) {
+    valor = valor.replace(/^(\d{3})(\d{3})(\d{0,3})/, '$1.$2.$3');
+  } else if (valor.length > 3) {
+    valor = valor.replace(/^(\d{3})(\d{0,3})/, '$1.$2');
+  }
+  input.value = valor;
+}
+
 function salvarCache(chave, dados) {
   try {
     const item = {
