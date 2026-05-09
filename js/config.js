98450066c33a2a2a5b0a2e8fff5cc43a02938427
ederramossupervisor <eder.ramos@educador.edu.es.gov.br>
2026-05-09 13:23:32 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 // CONFIGURAÇÕES GERAIS
 // =========================
 
-const API_URL = "https://script.google.com/macros/s/AKfycbzonYYSmXmy1UkmnZGou6bRfdJOoLUmrnw5fFfp1EU4cyB-EkGOmxzfxTA6LfDdc56_FA/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwjF4r4mnQRo48jDGxjH6po0hYrBbjwasR15pIEc71J4bsgWuuPG-708EBp21mWFrQUPw/exec";
 
 // Lista oficial de escolas (disponível para o supervisor)
 const LISTA_ESCOLAS = [
