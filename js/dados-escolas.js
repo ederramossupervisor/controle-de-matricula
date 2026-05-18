54eb1ee447a7ebfeaac7788bdc32d4aa79968f29
ederramossupervisor <eder.ramos@educador.edu.es.gov.br>
2026-05-18 13:45:07 -0300

    Create dados-escolas.js

---
+++ js/dados-escolas.js
@@ -0,0 +1,141 @@
+// =========================
+// DADOS COMPLEMENTARES DAS ESCOLAS
+// =========================
+const ESCOLAS_DADOS = {
+  "CEEFMTI Afonso Cláudio": {
+    city: "Afonso Cláudio",
+    director: "Allan Dyoni Dehete Many"
+  },
+  "CEEFMTI Elisa Paiva": {
+    city: "Conceição do Castelo",
+    director: "Rosangela Vargas Davel Pinto"
+  },
+  "EEEF Ivana Casagrande Scabelo": {
+    city: "Venda Nova do Imigrante",
+    director: "Maristela Broedel"
+  },
+  "EEEF Severino Paste": {
+    city: "Venda Nova do Imigrante",
+    director: "Maristela Broedel"
+  },
+  "EEEFM Alto Rio Possmoser": {
+    city: "Santa Maria de Jetibá",
+    director: "Adriana da Conceição Tesch"
+  },
+  "EEEFM Álvaro Castelo": {
+    city: "Brejetuba",
+    director: "Rose Fabrícia Moretto"
+  },
+  "EEEFM Domingos Perim": {
+    city: "Venda Nova do Imigrante",
+    director: "Maristela Broedel"
+  },
+  "EEEFM Elvira Barros": {
+    city: "Afonso Cláudio",
+    director: "Andrea Gomes Klug"
+  },
+  "EEEFM Fazenda Camporês": {
+    city: "Brejetuba",
+    director: "Emerson Ungarato"
+  },
+  "EEEFM Fazenda Emílio Schroeder": {
+    city: "Santa Maria de Jetibá",
+    director: "Jorge Schneider"
+  },
+  "EEEFM Fioravante Caliman": {
+    city: "Venda Nova do Imigrante",
+    director: "Celina Januário Moreira"
+  },
+  "EEEFM Frederico Boldt": {
+    city: "Santa Maria de Jetibá",
+    director: "David Felberg"
+  },
+  "EEEFM Gisela Salloker Fayet": {
+    city: "Domingos Martins",
+    director: "Maxwel Augusto Neves"
+  },
+  "EEEFM Graça Aranha": {
+    city: "Santa Maria de Jetibá",
+    director: "Camilo Pauli Dominicini"
+  },
+  "EEEFM Joaquim Caetano de Paiva": {
+    city: "Laranja da Terra",
+    director: "Miriam Klitzke Seibel"
+  },
+  "EEEFM José Cupertino": {
+    city: "Afonso Cláudio",
+    director: "Cléria Pagotto Ronchi Zanelato"
+  },
+  "EEEFM José Giestas": {
+    city: "Afonso Cláudio",
+    director: "Gederson Vargas Dazilio"
+  },
+  "EEEFM José Roberto Christo": {
+    city: "Afonso Cláudio",
+    director: "Andressa Silva Dias"
+  },
+  "EEEFM Leogildo Severiano de Souza": {
+    city: "Brejetuba",
+    director: "Adalberto Carlos Araújo Chaves"
+  },
+  "EEEFM Luiz Jouffroy": {
+    city: "Laranja da Terra",
+    director: "Nilza Abel Gumz"
+  },
+  "EEEFM Maria de Abreu Alvim": {
+    city: "Afonso Cláudio",
+    director: "Maria das Graças Fabio Costa"
+  },
+  "EEEFM Mário Bergamin": {
+    city: "Venda Nova do Imigrante",
+    director: "CELINA JANUÁRIO MOREIRA"  // nome em caixa alta conforme seu arquivo original
+  },
+  "EEEFM Marlene Brandão": {
+    city: "Brejetuba",
+    director: "Paulynne Ayres Tatagiba Gonçalves"
+  },
+  "EEEFM Pedra Azul": {
+    city: "Domingos Martins",
+    director: "Elizabeth Drumond Ambrósio Filgueiras"
+  },
+  "EEEFM Ponto do Alto": {
+    city: "Domingos Martins",
+    director: "Marcelo Ribett"
+  },
+  "EEEFM Profª Aldy Soares Merçon Vargas": {
+    city: "Conceição do Castelo",
+    director: "Israel Augusto Moreira Borges"
+  },
+  "EEEFM Prof Hermann Berger": {
+    city: "Santa Maria de Jetibá",
+    director: "Eliane Raasch Bicalho"
+  },
+  "EEEFM São Jorge": {
+    city: "Brejetuba",
+    director: "Jormi Maria da Silva"
+  },
+  "EEEFM São Luís": {
+    city: "Santa Maria de Jetibá",
+    director: "Valdirene Mageski Cordeiro Magri"
+  },
+  "EEEFM Teófilo Paulino": {
+    city: "Domingos Martins",
+    director: "Delfina Schneider Stein"
+  },
+  "EEEM Francisco Guilherme": {
+    city: "Santa Maria de Jetibá",
+    director: "Jonatas André Drescher"
+  },
+  "EEEM Mata fria": {
+    city: "Afonso Cláudio",
+    director: "Jonatas André Drescher"
+  },
+  "EEEFM Tutorial Teste": {
+    city: "Afonso Cláudio",
+    director: "Diretor Teste"
+  },
+  "EEEM Sobreiro": {
+    city: "Laranja da Terra",
+    director: "Jonatas André Drescher"
+  }
+};
